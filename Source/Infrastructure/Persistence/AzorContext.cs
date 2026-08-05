684b0555f05685578545389224b76cf99fe6d95e
euchico <franciscodepaula.web@gmail.com>
2026-08-04 22:59:29 -0300

    feat: Atualização de migrations.

---
+++ Source/Infrastructure/Persistence/AzorContext.cs
@@ -0,0 +1,26 @@
+using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public sealed class AzorContext(DbContextOptions<AzorContext> options) : DbContext(options)
+{
+    public DbSet<ApplicationUser> Users => Set<ApplicationUser>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<ApplicationUser>(user =>
+        {
+            user.ToTable("Users");
+            user.HasKey(applicationUser => applicationUser.Id);
+            user.Property(applicationUser => applicationUser.Id).HasMaxLength(450);
+            user.Property(applicationUser => applicationUser.UserName).HasMaxLength(256);
+            user.Property(applicationUser => applicationUser.NormalizedUserName).HasMaxLength(256);
+            user.Property(applicationUser => applicationUser.Email).HasMaxLength(256);
+            user.Property(applicationUser => applicationUser.NormalizedEmail).HasMaxLength(256);
+            user.Property(applicationUser => applicationUser.ConcurrencyStamp).IsConcurrencyToken();
+            user.HasIndex(applicationUser => applicationUser.NormalizedEmail).HasDatabaseName("EmailIndex");
+            user.HasIndex(applicationUser => applicationUser.NormalizedUserName).IsUnique().HasDatabaseName("UserNameIndex").HasFilter("[NormalizedUserName] IS NOT NULL");
+        });
+    }
+}
